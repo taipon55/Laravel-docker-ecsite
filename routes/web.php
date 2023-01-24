afc1d829e0bf0b098a54e9672bbb1addf3050a3b
taipon55 <Taisei127@outlook.com>
2023-01-24 10:37:53 +0900

    カート機能作成

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
 use App\Http\Controllers\ProductController;
-
+use App\Http\Controllers\LineItemController;
 
 Route::controller(ProductController::class)->group(function(){
     Route::name('product.')->group(function(){
         Route::get('/', 'index')->name('index');
         Route::get('/{id}','show')->name('show');
+    });
+});
+
+Route::controller(LineItemController::class)->group(function() { 
+    Route::name('line_item.')->group(function () {
+        Route::post('/line_item/create', 'create')->name('create');
     });
 });
